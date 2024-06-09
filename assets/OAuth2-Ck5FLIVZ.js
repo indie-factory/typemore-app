4d883703694a1ca1985b3ffb8358f36f48762012
Big.Lee <leekw3747@gmail.com>
2024-06-09 16:14:05 +0900

    rebuild

---
+++ assets/OAuth2-Ck5FLIVZ.js
@@ -0,0 +1,1 @@
+import{u as r}from"./vue-router-DceEJ2op.js";import{l as t,s}from"./index-t7pAO_uB.js";import{d as n}from"./@vue-mRSmcEt4.js";/* empty css             */import"./vuetify-C1e8o-wN.js";import"./axios-B6xwUs71.js";const l=n({__name:"OAuth2",setup(p){const e=r(),o=e.currentRoute.value.query.token;return o?(s(o),e.replace("/providers")):t(),()=>{}}});export{l as default};
---
+++ assets/ProviderApi-DPvaIvaQ.js
@@ -0,0 +1,1 @@
+import{i as r}from"./index-t7pAO_uB.js";const o=t=>r.post(`/providers/connect/${t}`),n=()=>r.get("/providers");export{o as c,n as g};
